541f79c9c8c0ab1e94a30b64fba64ff16a4f2345
Savanna <savanna.mclelland@gmail.com>
2026-07-27 23:51:15 -0600

    Added the init py files and need to separate the rest of the routes so app.py is easier to read

---
+++ routes/__init__.py
@@ -0,0 +1,4 @@
+''' Will remain empty but is created to make the point that route
+is treated like a package. This is necessary for the blueprint to
+work correctly.
+'''
---
+++ routes/home.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, render_template, request
+
+home_bp = Blueprint("home", __name__)
+
+
+@home_bp.route("/")
+def home():
+
+    protein_per_dollar = None
+    calorie_per_dollar = None
+    product_name = None
+    price = None
+    calories = None
+    protein = None
+
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        price = float(request.form['price'])
+        calories = float(request.form['calories'])
+        protein = float(request.form['protein'])
+
+        protein_per_dollar = protein / price if price != 0 else None
+        calorie_per_dollar = calories / price if price != 0 else None
+               
+    return render_template('index.html', protein_per_dollar=protein_per_dollar, calorie_per_dollar=calorie_per_dollar, product_name=product_name, price=price, calories=calories, protein=protein)
+
+
