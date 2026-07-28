22775da1469f8ccb81b002bbe6e1fda3c6911fd6
Savanna <savanna.mclelland@gmail.com>
2026-07-27 20:08:08 -0600

    Interactive with python and website

--- app.py
+++ app.py
@@ -6,13 +6,30 @@ access the food database. It will calculate the cost per meal based on serving s
 
 '''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+
+    protein_per_dollar = None
+    calorie_per_dollar = None
+    product_name = None
+    price = None
+    calories = None
+    protein = None
+
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        price = float(request.form['price'])
+        calories = float(request.form['calories'])
+        protein = float(request.form['protein'])
+
+        protein_per_dollar = protein / price if price != 0 else None
+        calorie_per_dollar = calories / price if price != 0 else None
+               
+    return render_template('index.html', protein_per_dollar=protein_per_dollar, calorie_per_dollar=calorie_per_dollar, product_name=product_name, price=price, calories=calories, protein=protein)
 
 @app.route('/about')
 def about():
