4f562f383d90293a54499d0d0fc1fffbe81cc93a
Savanna <savanna.mclelland@gmail.com>
2026-07-27 23:11:46 -0600

    Added a database

---
+++ database.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+from app import products
+
+def get_connection():
+    return sqlite3.connect("database/products.db")
+
+def create_table():
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        product_name TEXT NOT NULL,
+        price REAL NOT NULL,
+        calories REAL NOT NULL,
+        protein REAL NOT NULL
+    )
+    ''')
+
+    connection.commit()
+    connection.close()
+
+def add_product(product_name, price, calories, protein):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    INSERT INTO products (product_name, price, calories, protein)
+    VALUES (?, ?, ?, ?)
+    ''', (product_name, price, calories, protein))
+
+    connection.commit()
+    connection.close()
+
+def get_products():
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+
+    connection.close()
+    return products
+
+print(get_products())
